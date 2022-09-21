8b97b23e2db2736d864203cdc90997d213201fff
tropaadigselv <frederikahorup@gmail.com>
2022-09-21 09:43:17 +0200

    lavede Guitar færdig

--- projekter/Musik/Guitar/sketch.js
+++ projekter/Musik/Guitar/sketch.js
@@ -1,43 +1,108 @@
-let figureY;
-let speed;
+let CircleY;
+let CircleSpeed;
+let SquareY;
+let SquareSpeed;
+let CircleY2;
+let CircleSpeed2;
 let score;
+
+let note1;
+let note2;
+let note3;
+
+function preload() {
+  soundFormats("WAV", "mp3");
+  note1 = loadSound("Notes/C_Major7ShortGuitar_725");
+  note2 = loadSound("Notes/Gm_Minor7ShortGuitar_725");
+  note3 = loadSound("Notes/Em_Minor7Short1Guitar_725");
+}
 
 function setup() {
   createCanvas(500, 600);
-
-  figureY=50
-  speed = random(1, 5)
-  score=0
-
+  CircleY = 50;
+  CircleSpeed = random(1, 5);
+  SquareY = 50;
+  SquareSpeed = random(1, 5);
+  CircleY2 = 50;
+  CircleSpeed2 = random(1, 5);
+  score = 0;
 }
 
 function draw() {
-  figureY += speed;
- 
+  CircleY += CircleSpeed;
+  SquareY += SquareSpeed;
+  CircleY2 += CircleSpeed2;
+
   background(220);
   //guitar strenge
   strokeWeight(5);
-  for(let i =100; i<400; i+=50 ){
-    line(i,0,i,height);
+  for (let i = 100; i < 400; i += 50) {
+    line(i, 0, i, height);
   }
   stroke(180);
-  line(0,500,width,500);
+  line(0, 500, width, 500);
 
-  circle(100,figureY,50)
+  circle(100, CircleY, 50);
+  rectMode(CENTER);
+  rect(200, SquareY, 50);
+  circle(300, CircleY2, 50);
 
-    if((figureY > 500 && figureY <550) && keyIsDown(81)){
-      figureY = -50;
-      speed = random(1,5);
-      console.log("clicked")
-      score++;
-    }
-  
-    if(figureY>height+25){
-      figureY = -50;
-      speed = random(1,5);
-    }
-    
-    strokeWeight(0);
-    fill(0);
-    text(score,50,100)
+  if (CircleY > height + 25) {
+    CircleY = -50;
+    CircleSpeed = random(1, 5);
+    score--;
+  }
+  if (CircleY2 > height + 25) {
+    CircleY2 = -50;
+    CircleSpeed2 = random(1, 5);
+    score--;
+  }
+  if (SquareY > height + 25) {
+    SquareY = -50;
+    SquareSpeed = random(1, 5);
+    score--;
+  }
+
+  strokeWeight(0);
+  fill(0);
+  text(score, 50, 100);
+}
+
+function keyPressed() {
+  // for den første cirkle
+  if (keyCode == "81" && CircleY < 500) {
+    score--;
+    CircleY = -50;
+    CircleSpeed = random(1, 5);
+  }
+  if (CircleY > 500 && CircleY < height && keyCode == "81") {
+    CircleY = -50;
+    CircleSpeed = random(1, 10);
+    score++;
+    note1.play();
+  }
+  // for firkanten
+  if (keyCode == "87" && SquareY < 500) {
+    SquareY = -50;
+    SquareSpeed = random(1, 5);
+    score--;
+  }
+  if (SquareY > 500 && SquareY < height && keyCode == "87") {
+    SquareY = -50;
+    SquareSpeed = random(1, 10);
+    score++;
+    note2.play();
+  }
+  //for den anden cirkle
+  if (keyCode == "69" && CircleY2 < 500) {
+    score--;
+    CircleY2 = -50;
+    CircleSpeed2 = random(1, 5);
+  }
+  if (CircleY2 > 500 && CircleY2 < height && keyCode == "69") {
+    CircleY2 = -50;
+    CircleSpeed2 = random(1, 10);
+    score++;
+    note3.play();
+  }
 }
--- projekter/Musik/Trommer/sketch.js
+++ projekter/Musik/Trommer/sketch.js
@@ -3,68 +3,66 @@ let KickDrum;
 let HiHat;
 let Crash;
 let FloorDrum;
-let Drum
+let Drum;
 
-function preload(){
-  soundFormats('WAV','mp3');
-  SnareDrum=loadSound('Drums/snare');
-  KickDrum=loadSound('Drums/kick');
-  HiHat=loadSound('Drums/HiHat');
-  Crash=loadSound('Drums/crash');
-  FloorDrum=loadSound('Drums/floorDrum');
-  Drum=loadSound('Drums/Drum.mp3');
+function preload() {
+  soundFormats("WAV", "mp3");
+  SnareDrum = loadSound("Drums/snare");
+  KickDrum = loadSound("Drums/kick");
+  HiHat = loadSound("Drums/HiHat");
+  Crash = loadSound("Drums/crash");
+  FloorDrum = loadSound("Drums/floorDrum");
+  Drum = loadSound("Drums/Drum.mp3");
 }
-
-
 
 function setup() {
   createCanvas(600, 400);
   background(220);
 
   //laver trommesættet
-  circle(300,300,150);
-  ellipse(350,200,75,50);
-  ellipse(250,200,75,50);
-  ellipse(185,275,65,55);
-  ellipse(415,275,70,45);
-  fill(255,255,0);
-  ellipse(185,220,50,10)
-  ellipse(185,230,50,10)
-  ellipse(420,230,80,15)
+  circle(300, 300, 150);
+  ellipse(350, 200, 75, 50);
+  ellipse(250, 200, 75, 50);
+  ellipse(185, 275, 65, 55);
+  ellipse(415, 275, 70, 45);
+  fill(255, 255, 0);
+  ellipse(185, 220, 50, 10);
+  ellipse(185, 230, 50, 10);
+  ellipse(420, 230, 80, 15);
 }
 
 function draw() {
   //console.log('x ', mouseX, 'y ', mouseY)
-  
+
   // stortrommen
-  if(mouseIsPressed){
-  if(sqrt(sq(300-mouseX)+sq(300-mouseY))<150/2){
-    console.log('hej');
-    KickDrum.play();
-  }//lilletromme
-  else if((mouseX>151 && mouseX<217) && (mouseY> 247 && mouseY<302)){
-    console.log('hej');
-    SnareDrum.play();
-  }//venstre tromme over stortromme
-  else if((mouseX>212 && mouseX<287) && (mouseY> 174 && mouseY<224)){
-    Drum.play();
-    console.log('hej');
-  }// højre tromme over stortromme
-  else if((mouseX>311 && mouseX<387) && (mouseY> 174 && mouseY<224)){
-    Drum.play();
-    console.log('hej');
-  }//Floordrum
-  else if((mouseX>379 && mouseX<449) && (mouseY> 252 && mouseY<296)){
-    console.log('hej');
-    FloorDrum.play();
-  }//high-hat
-  else if((mouseX>160 && mouseX<209) && (mouseY> 214 && mouseY<235)){
-    console.log('hej');
-    HiHat.play();
-  }//bæken
-  else if((mouseX>379 && mouseX<459) && (mouseY> 222 && mouseY<236)){
-    console.log('hej');
-    Crash.play();
+  if (mouseIsPressed) {
+    if (sqrt(sq(300 - mouseX) + sq(300 - mouseY)) < 150 / 2) {
+      console.log("hej");
+      KickDrum.play();
+    } //lilletromme
+    else if (mouseX > 151 && mouseX < 217 && mouseY > 247 && mouseY < 302) {
+      console.log("hej");
+      SnareDrum.play();
+    } //venstre tromme over stortromme
+    else if (mouseX > 212 && mouseX < 287 && mouseY > 174 && mouseY < 224) {
+      Drum.play();
+      console.log("hej");
+    } // højre tromme over stortromme
+    else if (mouseX > 311 && mouseX < 387 && mouseY > 174 && mouseY < 224) {
+      Drum.play();
+      console.log("hej");
+    } //Floordrum
+    else if (mouseX > 379 && mouseX < 449 && mouseY > 252 && mouseY < 296) {
+      console.log("hej");
+      FloorDrum.play();
+    } //high-hat
+    else if (mouseX > 160 && mouseX < 209 && mouseY > 214 && mouseY < 235) {
+      console.log("hej");
+      HiHat.play();
+    } //bæken
+    else if (mouseX > 379 && mouseX < 459 && mouseY > 222 && mouseY < 236) {
+      console.log("hej");
+      Crash.play();
+    }
   }
-}
 }
