3c5db806a68a221174cc55feb82f4834b6f7eea6
tropaadigselv <frederikahorup@gmail.com>
2022-10-12 09:45:42 +0200

    løkke opgaver

--- kapitel 5/exicise 1/sketch.js
+++ kapitel 5/exicise 1/sketch.js
@@ -1,7 +1,14 @@
-function setup() {
-  createCanvas(400, 400);
-}
+//tegner cirkler tilfældege steder med tilfældige størrelser og numrer den så alt efter hvad i er i while-loopet
 
-function draw() {
-  background(220);
+function setup() {
+  let i = 0;
+  createCanvas(400, 400);
+  while (i < 100) {
+    let x = random(400);
+    let y = random(400);
+    let r = random(100);
+    circle(x, y, r);
+    text(i, x, y);
+    i = i + 1;
+  }
 }
--- kapitel 5/exicise 3/sketch.js
+++ kapitel 5/exicise 3/sketch.js
@@ -1,7 +1,12 @@
 function setup() {
   createCanvas(400, 400);
-}
-
-function draw() {
-  background(220);
+  for (i = 0; i < 100; i += 2) {
+    fill(255);
+    let x = random(400);
+    let y = random(400);
+    let r = random(100);
+    circle(x, y, r);
+    fill(255, 0, 0);
+    text(i, x, y);
+  }
 }
